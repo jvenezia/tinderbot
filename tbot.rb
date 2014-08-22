9e3d8d8ae5bef346d406e7611c77aef9a6ef4b99
Jeremy Venezia <veneziajeremy@gmail.com>
2014-08-22 02:20:20 +0200

    added human sleep and misc refactor

--- tbot.rb
+++ tbot.rb
@@ -3,13 +3,13 @@ require 'faraday_middleware'
 require 'watir-webdriver'
 require 'json'
 
-LIKED_TARGETS_LOG_PATH = 'liked_targets.txt'
+LIKED_PEOPLE_LOG_PATH = 'liked_people.txt'
 FACEBOOK_CREDENTIALS_PATH = 'facebook_credentials.json'
 
 FACEBOOK_AUTHENTICATION_TOKEN_URL = 'https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token'
+TINDER_API_URL = 'https://api.gotinder.com'
 
 def get_facebook_credentials
-  puts 'Getting facebook credentials...'
   browser = Watir::Browser.new
   browser.goto FACEBOOK_AUTHENTICATION_TOKEN_URL
   facebook_credentials = JSON.parse(File.open(FACEBOOK_CREDENTIALS_PATH).read)
@@ -28,8 +28,7 @@ def get_facebook_credentials
 end
 
 def get_tinder_connexion
-  puts 'Connecting to tinder...'
-  Faraday.new(url: 'https://api.gotinder.com') do |faraday|
+  Faraday.new(url: TINDER_API_URL) do |faraday|
     faraday.request :json
     faraday.adapter Faraday.default_adapter
   end
@@ -42,29 +41,40 @@ def sign_in(tinder_connexion, facebook_authentication_token, facebook_user_id)
   tinder_connexion.headers['X-Auth-Token'] = tinder_authentication_token
 end
 
-facebook_authentication_token, facebook_user_id = get_facebook_credentials
+def get_new_recommended_people_set(tinder_connexion)
+  JSON.parse(tinder_connexion.post('user/recs').body)['results']
+end
 
+def human_sleep
+  sleep(rand(0.1..0.5))
+end
+
+def like_people(tinder_connexion)
+  begin
+    liked_people_log_file = File.open(LIKED_PEOPLE_LOG_PATH, 'a')
+    recommended_people_set = get_new_recommended_people_set(tinder_connexion)
+    liked_person_count = 0
+    while recommended_people_set
+      recommended_people_set.each do |person|
+        human_sleep
+        person_id = person['_id']
+        tinder_connexion.get 'like/' + person_id
+        liked_people_log_file.write("#{person_id}\n")
+        liked_person_count += 1
+        puts person['name']
+      end
+      recommended_people_set = get_new_recommended_people_set(tinder_connexion)
+    end
+  ensure
+    puts "\nLiked #{liked_person_count} target(s)."
+    liked_people_log_file.close if liked_people_log_file
+  end
+end
+
+puts 'Getting facebook credentials...'
+facebook_authentication_token, facebook_user_id = get_facebook_credentials
+puts 'Connecting to tinder...'
 tinder_connexion = get_tinder_connexion
 sign_in(tinder_connexion, facebook_authentication_token, facebook_user_id)
-
-puts 'Fetching targets...'
-begin
-  liked_targets_log_file = File.open(LIKED_TARGETS_LOG_PATH, 'a')
-  recommended_targets = JSON.parse(tinder_connexion.post('user/recs').body)
-  liked_targets_count = 0
-  while recommended_targets['results']
-    puts 'Liking...'
-    recommended_targets['results'].each do |target|
-      target_id = target['_id']
-      liked_targets_log_file.write("#{target_id}\n")
-      tinder_connexion.get 'like/' + target_id
-      liked_targets_count += 1
-      puts "Liked #{target['name']}."
-    end
-    recommended_targets = JSON.parse(tinder_connexion.post('user/recs').body)
-  end
-  puts "\nLiked #{liked_targets_count} target(s)."
-ensure
-  puts 'Ending...'
-  liked_targets_log_file.close if liked_targets_log_file
-end
+puts 'Starting likes...'
+like_people(tinder_connexion)
